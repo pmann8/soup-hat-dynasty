05fad00081ab78b71dc0a9e2a1dd692e57ecbb18
Patrick Mann <patrick.mann8@gmail.com>
2025-03-26 14:35:54 -0400

    https://github.com/nmelhado/league-page/commit/92bc81a146b128ba946e6a96c0fc94d377f514dd#diff-8da72075b0cc415fbf41625c0b1de95901af45f373d1e7df5d88538c9c4f81be

--- src/lib/utils/helperFunctions/leagueRecords.js
+++ src/lib/utils/helperFunctions/leagueRecords.js
@@ -465,7 +465,6 @@ const digestBracket = ({bracket, playoffRecords, playoffRounds, matchupDifferent
 
 		postSeasonData = meshPostSeasonData(postSeasonData, pSD);
 
-		postSeasonData = meshPostSeasonData(postSeasonData, pSD);
 		seasonPointsRecord = sPR;
 		matchupDifferentials = mD;
 	}
