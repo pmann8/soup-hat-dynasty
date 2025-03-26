4c5e1bc05dcddc5c4d8e220f35bc0db421b7ad5e
Patrick Mann <patrick.mann8@gmail.com>
2025-03-26 14:51:38 -0400

    https://github.com/nmelhado/league-page/commit/33cecb359afa70428ca2efed9218e226bb8b4a83

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,15 @@
 <!-- __layout.svelte -->
 <script>
 	import { Nav, Footer } from "$lib/components"
+    import { dev } from '$app/environment';
+    import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+    injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
 <main>
     <Nav /> <!-- adds the nav (small and large) -->
-  
+
     <slot />
 
     <Footer /> <!-- adds the footer -->
