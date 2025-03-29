8996175a0744f34dc1a121a11097dc32a1fbbf56
Patrick Mann <patrick.mann8@gmail.com>
2025-03-29 12:34:17 -0400

    Update the standings table

--- src/routes/standings/+page.svelte
+++ src/routes/standings/+page.svelte
@@ -2,7 +2,7 @@
 	import { Standings } from '$lib/components'
 
 	export let data;
-	const {standingsData, leagueTeamManagersData, allTimeStandingsData} = data;
+	const {standingsData, leagueTeamManagersData} = data;
 </script>
 
 <style>
@@ -14,5 +14,5 @@
 </style>
 
 <div class="holder">
-	<Standings {standingsData} {leagueTeamManagersData} {allTimeStandingsData} />
+	<Standings {standingsData} {leagueTeamManagersData} />
 </div>
