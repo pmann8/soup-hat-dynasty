357f3a15fe2f25ee90fe93df46014ce3bccc1fa2
Patrick Mann <patrick.mann8@gmail.com>
2026-07-22 15:00:58 -0400

    update league ID to reflect new league information

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "1217835949778542592"; // your league ID
+export const leagueID = "1319070237999534080"; // your league ID
 export const leagueName = "Soup Hat Dynasty"; // your league name
 export const dues = 50; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
